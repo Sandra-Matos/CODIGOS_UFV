d1842ff51c7c3a8aa5cf49d80157bc0b3fca47e4
Sandra-Matos <sandra.matos@ufv.br>
2021-03-09 18:18:40 -0300

    EXERCICIO_02.c
    Faça um programa para ler 03 números reais do teclado e verificar se o primeiro é maior que a soma dos outros dois.

---
+++ LISTA01/EXERCICIO_02.c
@@ -0,0 +1,20 @@
+//Faça um programa para ler 03 números reais do teclado e verificar se o primeiro é maior 
+//que a soma dos outros dois.
+#include<stdio.h>
+#include<stdlib.h>
+int main (){
+float n1,n2,n3, soma;
+printf("Digite tres valores para descobrir se o primeiro valor e maior que a soma dos dois ultimos.");
+scanf("%f",&n1);
+scanf("%f",&n2);
+scanf("%f",&n3);
+soma = n2+n3;
+
+if(n1>soma){
+     printf("O primeiro valor e maior que a soma dos outros dois.");
+}else
+    printf("O primeiro valor nao e amior que a soma dos outros dois.");
+
+    
+return 0;
+}
