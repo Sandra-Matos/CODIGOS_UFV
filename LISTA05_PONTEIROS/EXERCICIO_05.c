8845e6a568032bfad7bee15853c967d98475ce52
Sandra-Matos <sandra.matos@ufv.br>
2021-03-28 10:02:23 -0300

    alterações

--- LISTA05_PONTEIROS/EXERCICIO_05.c
+++ LISTA05_PONTEIROS/EXERCICIO_05.c
@@ -2,7 +2,7 @@
 //impresso.
 //main () {
 //int i = 5, *p = &i;
-//printf (“%d %d %d %d %d \n”, p, *p+2, **&p, 3**p, **&p+4);}
+//printf (“%d %d %d %d %d \n”, p, *p+2, **&p, 3**p,**&p+4);} 
 #include <stdio.h>
 #include <stdlib.h>
 main () {
@@ -12,7 +12,9 @@ printf (" %d %d %d %d %d \n ", p, *p+2, **&p, 3**p, **&p+4);
 return 0;
 // p está imprimindo o endereço de memoria de i.
 //*p+2 está imprimindo o valor de i que é 5 e somando com 2, resultando em 7.
-//
+//**&p esta imprimindo o valor de um ponteiro apontando para outro, resultando em 5.
+//3**&p esta imprimindo o valor de 3 sendo multiplicado pelo valor de i, resultando em 15
+//**&p+4 esta imprimindo o valor do valor de i que 5 + 4 resultando em 9.
 
 
 }
